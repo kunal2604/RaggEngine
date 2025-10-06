8d1d2f820537349accedb2572efeb7c6963d5c95
Kunal Kumar <kmrknl2604@gmail.com>
2025-10-06 16:37:32 +0530

    Beginning - Project Setup

---
+++ RagengineEditor/GameProject/OpenProjectView.xaml.cs
@@ -0,0 +1,15 @@
+﻿﻿using System.Windows.Controls;
+
+namespace RagengineEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for OpenProjectView.xaml
+    /// </summary>
+    public partial class OpenProjectView : UserControl
+    {
+        public OpenProjectView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ RagengineEditor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace RagengineEditor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender == openProjectButton)
+            {
+                if(createProjectButton.IsChecked == true)
+                {
+                    createProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(0);
+                }
+                openProjectButton.IsChecked = true;
+            }
+            else
+            {
+                if (openProjectButton.IsChecked == true)
+                {
+                    openProjectButton.IsChecked = false;
+                    browserContent.Margin = new Thickness(-800,0,0,0);
+                }
+                createProjectButton.IsChecked = true;
+            }
+        }
+    }
+}
