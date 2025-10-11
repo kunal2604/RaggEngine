8cdc6cf12c796f3bc9725bb4808ff049d1d76311
Kunal Kumar <kmrknl2604@gmail.com>
2025-10-11 08:40:37 +0530

    Create new project, generate .ragengine file in new project

--- RagengineEditor/Common/ViewModelBase.cs
+++ RagengineEditor/Common/ViewModelBase.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel;
+using System.Runtime.Serialization;
 
 namespace RagengineEditor
 {
+    [DataContract(IsReference = true)]
     public class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
---
+++ RagengineEditor/GameProject/Project.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+
+namespace RagengineEditor.GameProject
+{
+    [DataContract(Name = "Game")]
+    public class Project : ViewModelBase
+    {
+        public static string Extension { get; } = ".ragengine";
+        [DataMember]
+        public string ProjectName { get; private set; }
+        [DataMember]
+        public string ProjectPath { get; private set; }
+        public string FullPath => System.IO.Path.Combine(ProjectPath, ProjectName + Extension);
+        [DataMember(Name = "Scenes")]
+        private ObservableCollection<Scene> _scenes = new ObservableCollection<Scene>();
+        public ReadOnlyObservableCollection<Scene> Scenes { get; }
+        public Project(string projectName, string projectPath)
+        {
+            ProjectName = projectName;
+            ProjectPath = projectPath;
+            _scenes.Add(new Scene(this, "DefaultScene"));
+        }
+    }
+}
